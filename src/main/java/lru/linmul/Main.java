f3eeb72bc6b2351b20d5abeb9edad884b75702f9
A. MULCEY <amaury.mulcey@etudiant.univ-lr.fr>
2024-12-27 22:24:37 +0100

    ajout d'une icone

--- src/main/java/lru/linmul/Main.java
+++ src/main/java/lru/linmul/Main.java
@@ -4,49 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        /**CSV csv = new CSV("C:/users/amaur/Downloads/od_parking_dispo.csv");
-        Parking parking = null;
-        for (String[] row : csv.readAll()) {
-            //String[] tempdate = row[6].split(" ")[0].split("-");
-            parking = new Parking(
-                    row[0],
-                    row[1],
-                    Double.parseDouble(row[2]),
-                    Double.parseDouble(row[3]),
-                    Double.parseDouble(row[4]),
-                    Double.parseDouble(row[5]),
-                    //new Date(tempdate[0], tempdate[1], tempdate[2]),
-                    Integer.parseInt(row[7]),
-                    Integer.parseInt(row[8])
-            );
-        }
-
-        for (Parking p : Parking.getParkings()) {
-            System.out.println(p);
-        }
-
-        System.out.println(parking.getLessOccupied());
-
-        System.out.println(parking.getMostOccupied());
-
-        System.out.println(parking.searchParking("Les Salines"));*/
+    public static void main( String[] args ) {
         launch(args);
-
     }
 
     @Override
-    public void start(Stage mainStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/lru/linmul/scenes/Scene.fxml"));
+    public void start( Stage mainStage ) throws Exception {
+        FXMLLoader loader = new FXMLLoader( getClass().getResource( "/lru/linmul/scenes/Scene.fxml" ) );
+
         Parent root = loader.load();
+
         Scene scene = new Scene(root);
-        mainStage.setTitle("ParkingLR");
-        mainStage.setScene(scene);
-        mainStage.setResizable(false);
+
+        mainStage.getIcons().add( new Image( Objects.requireNonNull( getClass().getResourceAsStream( "/lru/linmul/lru.png" ) ) ) );
+        mainStage.setTitle( "ParkingLR" );
+        mainStage.setScene( scene );
+        mainStage.setResizable( false );
         mainStage.show();
     }
 }
